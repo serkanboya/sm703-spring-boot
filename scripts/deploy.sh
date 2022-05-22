86ee6f66c5a8772f078c16bec67903ae85fbf6a1
kircicegikorkmaz <kircicegi@toptal.com>
2022-05-22 08:58:47 +0300

    cont

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 cd $(dirname $0)
 mv complete/target/*.war /Users/kircicegikorkmaz/dev/apache-tomcat-8.5.49/webapps/sm703.war
-/Users/kircicegikorkmaz/dev/apache-tomcat-8.5.49/bin/shutdown.sh
-/Users/kircicegikorkmaz/dev/apache-tomcat-8.5.49/bin/startup.sh
 
