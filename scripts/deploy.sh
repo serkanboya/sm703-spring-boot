04528aeb9ec1dec569789be62a8fe949d0572aef
kircicegikorkmaz <kircicegi@toptal.com>
2022-05-22 09:02:33 +0300

    war name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd $(dirname $0)
-mv complete/target/*.war /Users/kircicegikorkmaz/dev/apache-tomcat-8.5.49/webapps/sm703.war
+mv complete/target/spring-boot-complete-0.0.1-SNAPSHOT.war /Users/kircicegikorkmaz/dev/apache-tomcat-8.5.49/webapps/sm703.war
 
